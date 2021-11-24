1b44dab3931d639ecc5646159b2744507706d8c8
Victor Peixoto <vmp@uvic.ca>
2021-11-23 23:52:44 -0300

    Extract functions

--- src/cpf.js
+++ src/cpf.js
@@ -1,15 +1,19 @@
+function hasValidFormat(str) {
+    return /^((\d{3}\.\d{3}\.\d{3}-\d\d)|\d{11})$/.exec(str);
+}
+
+function removeSymbols(str) {
+    return str.replace(/\D/g, '');
+}
+
 function validate(str) {
     if (str === null)
         return false;
     if (str === undefined)
         return false;
-    if (str.length < 11 || str.length > 14)
+    if (!hasValidFormat(str))
         return false;
-    str = str
-        .replace('.', '')
-        .replace('.', '')
-        .replace('-', '')
-        .replace(" ", "");
+    str = removeSymbols(str);
     if (str.split("").every(c => c === str[0]))
         return false;
     let [d1, d2] = [0, 0];
@@ -24,9 +28,8 @@ function validate(str) {
     rest = d2 % 11;
     const dg2 = rest < 2 ? 0 : 11 - rest;
     const nDigVerific = str.substring(str.length - 2, str.length);
-    const nDigResult = "" + dg1 + "" + dg2;
+    const nDigResult = `${dg1}${dg2}`;
     return nDigVerific == nDigResult;
-
 }
 
 module.exports = { validate };
