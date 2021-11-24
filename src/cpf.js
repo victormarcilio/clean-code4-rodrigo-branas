5d457cf7dea72af0661fbbdbd632aacc378ab7bd
Victor Peixoto <vmp@uvic.ca>
2021-11-23 23:09:08 -0300

    Fix declaration/initialization places and constness

--- src/cpf.js
+++ src/cpf.js
@@ -12,24 +12,19 @@ function validate(str) {
         .replace(" ", "");
     if (str.split("").every(c => c === str[0]))
         return false;
-    let d1, d2;
-    let dg1, dg2, rest;
-    let digito;
-    let nDigResult;
-    d1 = d2 = 0;
-    dg1 = dg2 = rest = 0;
+    let [d1, d2] = [0, 0];
     for (let nCount = 1; nCount < str.length - 1; nCount++) {
-        digito = parseInt(str.substring(nCount - 1, nCount));
+        const digito = parseInt(str[nCount - 1]);
         d1 = d1 + (11 - nCount) * digito;
         d2 = d2 + (12 - nCount) * digito;
     }
-    rest = d1 % 11;
-    dg1 = (rest < 2) ? 0 : 11 - rest;
+    let rest = d1 % 11;
+    const dg1 = (rest < 2) ? 0 : 11 - rest;
     d2 += 2 * dg1;
     rest = d2 % 11;
-    dg2 = rest < 2 ? 0 : 11 - rest;
-    let nDigVerific = str.substring(str.length - 2, str.length);
-    nDigResult = "" + dg1 + "" + dg2;
+    const dg2 = rest < 2 ? 0 : 11 - rest;
+    const nDigVerific = str.substring(str.length - 2, str.length);
+    const nDigResult = "" + dg1 + "" + dg2;
     return nDigVerific == nDigResult;
 
 }
