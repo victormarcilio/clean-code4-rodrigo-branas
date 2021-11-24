e04d2587bbded0b7712a23e8deb11e56e38d648b
Victor Peixoto <vmp@uvic.ca>
2021-11-23 22:59:19 -0300

    Remove try/catch block and its respective test

--- src/cpf.js
+++ src/cpf.js
@@ -12,30 +12,26 @@ function validate(str) {
         .replace(" ", "");
     if (str.split("").every(c => c === str[0]))
         return false;
-    try {
-        let d1, d2;
-        let dg1, dg2, rest;
-        let digito;
-        let nDigResult;
-        d1 = d2 = 0;
-        dg1 = dg2 = rest = 0;
-        for (let nCount = 1; nCount < str.length - 1; nCount++) {
-            digito = parseInt(str.substring(nCount - 1, nCount));
-            d1 = d1 + (11 - nCount) * digito;
-            d2 = d2 + (12 - nCount) * digito;
-        }
-        rest = d1 % 11;
-        dg1 = (rest < 2) ? 0 : 11 - rest;
-        d2 += 2 * dg1;
-        rest = d2 % 11;
-        dg2 = rest < 2 ? 0 : 11 - rest;
-        let nDigVerific = str.substring(str.length - 2, str.length);
-        nDigResult = "" + dg1 + "" + dg2;
-        return nDigVerific == nDigResult;
-    } catch (e) {
-        console.error("Erro !" + e);
-        return false;
+    let d1, d2;
+    let dg1, dg2, rest;
+    let digito;
+    let nDigResult;
+    d1 = d2 = 0;
+    dg1 = dg2 = rest = 0;
+    for (let nCount = 1; nCount < str.length - 1; nCount++) {
+        digito = parseInt(str.substring(nCount - 1, nCount));
+        d1 = d1 + (11 - nCount) * digito;
+        d2 = d2 + (12 - nCount) * digito;
     }
+    rest = d1 % 11;
+    dg1 = (rest < 2) ? 0 : 11 - rest;
+    d2 += 2 * dg1;
+    rest = d2 % 11;
+    dg2 = rest < 2 ? 0 : 11 - rest;
+    let nDigVerific = str.substring(str.length - 2, str.length);
+    nDigResult = "" + dg1 + "" + dg2;
+    return nDigVerific == nDigResult;
+
 }
 
 module.exports = { validate };
--- src/cpf.test.js
+++ src/cpf.test.js
@@ -1,5 +1,4 @@
 const {validate} = require("./cpf");
-const sinon = require("sinon");
 
 test("Must return false when parameter is null", () => {
     const cpf = null;
@@ -41,11 +40,3 @@ test("Must return true for a valid CPF", () => {
     expect(isValid2).toBeTruthy();
 });
 
-test("Must return false when an exception is thrown", () => {
-    const cpf = "558.155.780-29";
-    const parseIntStub = sinon.stub(global, "parseInt");
-    parseIntStub.throws(new Error({a : 1, b : 2}));
-    const isValid = validate(cpf);
-    expect(isValid).toBeFalsy();
-    parseIntStub.restore();
-});
