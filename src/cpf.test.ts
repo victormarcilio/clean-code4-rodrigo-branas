ffdcedb1a9cf6422aee976e7bd794c79ace611ea
Victor Peixoto <vmp@uvic.ca>
2021-11-25 19:58:12 -0300

    Extract the CPF class

--- src/cpf.test.ts
+++ src/cpf.test.ts
@@ -1,42 +1,49 @@
-import { calculateDigit, validate } from "./cpf";
+import { CPF } from "./cpf";
 
 test("Must return false when cpf has less than 11 characters", () => {
-    const cpf = "0000";
-    const isValid = validate(cpf);
+    const cpf_input = "0000";
+    const cpf = new CPF(cpf_input);
+    const isValid = cpf.validate(cpf_input);
     expect(isValid).toBeFalsy();
 });
 
 test("Must return false when cpf has more than 14 characters", () => {
-    const cpf = "0000000000000000";
-    const isValid = validate(cpf);
+    const cpf_input = "0000000000000000";
+    const cpf = new CPF(cpf_input);
+    const isValid = cpf.validate(cpf_input);
     expect(isValid).toBeFalsy();
 });
 
 test("Must return false when all digits are the same", () => {
-    const cpf = "000.000.000-00";
-    const isValid = validate(cpf);
+    const cpf_input = "000.000.000-00";
+    const cpf = new CPF(cpf_input);
+    const isValid = cpf.validate(cpf_input);
     expect(isValid).toBeFalsy();
 });
 
 test("Must return true for a valid CPF", () => {
-    const cpf = "628.763.840-00";
-    const cpf2 = "558.155.780-29";
-    const isValid = validate(cpf);
-    const isValid2 = validate(cpf2);
+    const cpf_input = "628.763.840-00";
+    const cpf_input2 = "558.155.780-29";
+    const cpf = new CPF(cpf_input);
+    const cpf2 = new CPF(cpf_input2);
+    const isValid = cpf.validate(cpf_input);
+    const isValid2 = cpf.validate(cpf_input2);
     expect(isValid).toBeTruthy();
     expect(isValid2).toBeTruthy();
 });
 
 test("Must calculate correct digits", () => {
-    const cpf = "55815578029";
-    const digit1 = calculateDigit(cpf.substr(0, 9));
+    const cpf_input = "55815578029";
+    const cpf = new CPF(cpf_input);
+    const digit1 = cpf.calculateDigit(cpf_input.substr(0, 9));
     expect(digit1).toBe('2');
-    const digit2 = calculateDigit(cpf.substr(0, 10));
+    const digit2 = cpf.calculateDigit(cpf_input.substr(0, 10));
     expect(digit2).toBe('9');
 });
 
 test("Must return false when symbols are misplaced", () => {
-    const cpf = "-..62876384000";
-    const isValid = validate(cpf);
+    const cpf_input = "-..62876384000";
+    const cpf = new CPF(cpf_input);
+    const isValid = cpf.validate(cpf_input);
     expect(isValid).toBeFalsy();
 });
--- src/cpf.ts
+++ src/cpf.ts
@@ -1,32 +1,39 @@
-function hasValidFormat(str: string) {
-    return /^((\d{3}\.\d{3}\.\d{3}-\d\d)|\d{11})$/.exec(str);
-}
+export class CPF {
+    private readonly digits: string;
 
-function removeSymbols(str: string) {
-    return str.replace(/\D/g, '');
-}
+    constructor(cpf_input: string) {
+        this.digits = this.removeSymbols(cpf_input);
+    }
+    hasValidFormat(str: string) {
+        return /^((\d{3}\.\d{3}\.\d{3}-\d\d)|\d{11})$/.exec(str);
+    }
 
-export function calculateDigit(cpf: string) {
-    let acc = 0;
-    for (let i = cpf.length - 1, multiply = 2; i >= 0; multiply++, i--)
-        acc += multiply * parseInt(cpf[i]);
-    const remainder = acc % 11;
-    return remainder < 2 ? '0' : `${11 - remainder}`;
-}
+    removeSymbols(str: string) {
+        return str.replace(/\D/g, '');
+    }
 
-function checkVerifierDigits(cpf: string) {
-    const digit1 = calculateDigit(cpf.substr(0, 9));
-    const digit2 = calculateDigit(cpf.substr(0, 10));
-    return digit1 + digit2 == cpf.substr(cpf.length - 2);
-}
+    calculateDigit(cpf: string) {
+        let acc = 0;
+        for (let i = cpf.length - 1, multiply = 2; i >= 0; multiply++, i--)
+            acc += multiply * parseInt(cpf[i]);
+        const remainder = acc % 11;
+        return remainder < 2 ? '0' : `${11 - remainder}`;
+    }
 
-function hasDifferentChars(str: string) {
-    return str.split("").some(c => c !== str[0]);
-}
+    checkVerifierDigits(cpf: string) {
+        const digit1 = this.calculateDigit(cpf.substr(0, 9));
+        const digit2 = this.calculateDigit(cpf.substr(0, 10));
+        return digit1 + digit2 == cpf.substr(cpf.length - 2);
+    }
 
-export function validate(str: string) {
-    if (!hasValidFormat(str))
-        return false;
-    str = removeSymbols(str);
-    return hasDifferentChars(str) && checkVerifierDigits(str);
+    hasDifferentChars(str: string) {
+        return str.split("").some(c => c !== str[0]);
+    }
+
+    validate(str: string) {
+        if (!this.hasValidFormat(str))
+            return false;
+        str = this.removeSymbols(str);
+        return this.hasDifferentChars(str) && this.checkVerifierDigits(str);
+    }
 }
