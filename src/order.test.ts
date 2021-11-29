cfedc511dff28d300e26a4414b194fe44cb4d7fa
Victor Peixoto <vmp@uvic.ca>
2021-11-28 21:30:13 -0300

    Add tests for orders with discounts

--- src/order.test.ts
+++ src/order.test.ts
@@ -1,16 +1,16 @@
 import { CPF } from "./cpf";
 import { Order } from "./order";
-import { Coupon, CouponTYpe } from "./coupon";
+import { Coupon, CouponType } from "./coupon";
 import { Item } from "./item";
 
 test("Orders with invalid cpf are not allowed", () => {
     const cpf_input = "0000";
-    expect(() => new Order(new CPF(cpf_input), new Coupon("Black Friday", CouponTYpe.Fix, 20))).toThrow();
+    expect(() => new Order(new CPF(cpf_input), new Coupon("Black Friday", CouponType.Fix, 20))).toThrow();
 });
 
 test("Should allow orders with valid cpf", () => {
     const cpf_input = "558.155.780-29";
-    const order = new Order(new CPF(cpf_input), new Coupon("Black Friday", CouponTYpe.Fix, 20));
+    const order = new Order(new CPF(cpf_input), new Coupon("Black Friday", CouponType.Fix, 20));
     expect(order).toBeDefined();
 });
 
@@ -21,4 +21,32 @@ test("Should correctly calculate total for orders with 3 different items and amo
     order.addItem(new Item("arroz", 193, 8));
     order.addItem(new Item("feijao", 200, 10), 5);
     expect(order.getTotal()).toBe(88);
+});
+
+test("Should correctly calculate a percentage discount", () => {
+    const cpf_input = "558.155.780-29";
+    const order = new Order(new CPF(cpf_input), new Coupon("Black Friday", CouponType.Percentage, 50));
+    order.addItem(new Item("gasolina", 666, 15), 2);
+    order.addItem(new Item("arroz", 193, 8));
+    order.addItem(new Item("feijao", 200, 10), 5);
+    expect(order.getTotal()).toBe(44);
+});
+
+test("Should correctly calculate a fix discount", () => {
+    const cpf_input = "558.155.780-29";
+    const order = new Order(new CPF(cpf_input), new Coupon("Black Friday", CouponType.Fix, 50));
+    order.addItem(new Item("gasolina", 666, 15), 2);
+    order.addItem(new Item("arroz", 193, 8));
+    order.addItem(new Item("feijao", 200, 10), 5);
+    expect(order.getTotal()).toBe(38);
+});
+
+
+test("Should not end up with a negative total by applying a fix discount higher than the total", () => {
+    const cpf_input = "558.155.780-29";
+    const order = new Order(new CPF(cpf_input), new Coupon("Black Friday", CouponType.Fix, 100));
+    order.addItem(new Item("gasolina", 666, 15), 2);
+    order.addItem(new Item("arroz", 193, 8));
+    order.addItem(new Item("feijao", 200, 10), 5);
+    expect(order.getTotal()).toBe(0);
 });
