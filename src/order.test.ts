3b2c75215a6483b716019af9379ae23eaca5a9af
Victor Peixoto <vmp@uvic.ca>
2021-12-02 10:52:46 -0300

    Refactor Order.test

--- src/order.test.ts
+++ src/order.test.ts
@@ -3,76 +3,74 @@ import { Order } from "./order";
 import { Coupon, CouponType } from "./coupon";
 import { Item } from "./item";
 
-let expirationDate: Date;
-beforeEach(() => expirationDate = new Date())
+let unexpiredDate: Date;
+let expiredDate: Date;
+const validCPF: CPF = new CPF("558.155.780-29");
+const PRODUCT_1_COSTING_15 = new Item("Product 1", 1, 15);
+const PRODUCT_2_COSTING_8 = new Item("Product 2", 2, 8);
+const PRODUCT_3_COSTING_10 = new Item("Product 3", 3, 10);
+
+beforeAll(() => {
+    unexpiredDate = new Date();
+    unexpiredDate.setMonth(unexpiredDate.getMonth() + 1);
+    expiredDate = new Date();
+    expiredDate.setMonth(unexpiredDate.getMonth() - 1);
+});
 
 test("Orders with invalid cpf are not allowed", () => {
-    const cpf_input = "0000";
-    expect(() => new Order(new CPF(cpf_input))).toThrow();
+    const invalidCPFInput = "313.543.312-43";
+    expect(() => new Order(new CPF(invalidCPFInput))).toThrow();
 });
 
 test("Should allow orders with valid cpf", () => {
-    const cpf_input = "558.155.780-29";
-    const order = new Order(new CPF(cpf_input));
+    const order = new Order(validCPF);
     expect(order).toBeDefined();
 });
 
 test("Should correctly calculate total for orders with 3 different items and amounts", () => {
-    const cpf_input = "558.155.780-29";
-    const order = new Order(new CPF(cpf_input));
-    order.addItem(new Item("gasolina", 666, 15), 2);
-    order.addItem(new Item("arroz", 193, 8));
-    order.addItem(new Item("feijao", 200, 10), 5);
+    const order = new Order(validCPF);
+    order.addItem(PRODUCT_1_COSTING_15, 2);
+    order.addItem(PRODUCT_2_COSTING_8);
+    order.addItem(PRODUCT_3_COSTING_10, 5);
     expect(order.getTotal()).toBe(88);
 });
 
 test("Should correctly calculate a percentage discount", () => {
-    const cpf_input = "558.155.780-29";
-    expirationDate.setMonth(expirationDate.getMonth() + 1);
-    const order = new Order(new CPF(cpf_input), new Coupon("Black Friday", CouponType.Percentage, 50, expirationDate));
-    order.addItem(new Item("gasolina", 666, 15), 2);
-    order.addItem(new Item("arroz", 193, 8));
-    order.addItem(new Item("feijao", 200, 10), 5);
+    const order = new Order(validCPF, new Coupon("Black Friday", CouponType.Percentage, 50, unexpiredDate));
+    order.addItem(PRODUCT_1_COSTING_15, 2);
+    order.addItem(PRODUCT_2_COSTING_8);
+    order.addItem(PRODUCT_3_COSTING_10, 5);
     expect(order.getTotal()).toBe(44);
 });
 
 test("Should correctly calculate a fixed discount", () => {
-    const cpf_input = "558.155.780-29";
-    expirationDate.setMonth(expirationDate.getMonth() + 1);
-    const order = new Order(new CPF(cpf_input), new Coupon("Black Friday", CouponType.Fixed, 50, expirationDate));
-    order.addItem(new Item("gasolina", 666, 15), 2);
-    order.addItem(new Item("arroz", 193, 8));
-    order.addItem(new Item("feijao", 200, 10), 5);
+    const order = new Order(validCPF, new Coupon("Black Friday", CouponType.Fixed, 50, unexpiredDate));
+    order.addItem(PRODUCT_1_COSTING_15, 2);
+    order.addItem(PRODUCT_2_COSTING_8);
+    order.addItem(PRODUCT_3_COSTING_10, 5);
     expect(order.getTotal()).toBe(38);
 });
 
-
 test("Should not end up with a negative total by applying a fix discount higher than the total", () => {
-    const cpf_input = "558.155.780-29";
-    expirationDate.setMonth(expirationDate.getMonth() + 1);
-    const order = new Order(new CPF(cpf_input), new Coupon("Black Friday", CouponType.Fixed, 100, expirationDate));
-    order.addItem(new Item("gasolina", 666, 15), 2);
-    order.addItem(new Item("arroz", 193, 8));
-    order.addItem(new Item("feijao", 200, 10), 5);
+    const order = new Order(validCPF, new Coupon("Black Friday", CouponType.Fixed, 100, unexpiredDate));
+    order.addItem(PRODUCT_1_COSTING_15, 2);
+    order.addItem(PRODUCT_2_COSTING_8);
+    order.addItem(PRODUCT_3_COSTING_10, 5);
     expect(order.getTotal()).toBe(0);
 });
 
 test("Should give discount for an expired percentage discount coupon", () => {
-    const cpf_input = "558.155.780-29";
-    expirationDate.setFullYear(expirationDate.getFullYear() - 1);
-    const order = new Order(new CPF(cpf_input), new Coupon("Black Friday", CouponType.Percentage, 50, expirationDate));
-    order.addItem(new Item("gasolina", 666, 15), 2);
-    order.addItem(new Item("arroz", 193, 8));
-    order.addItem(new Item("feijao", 200, 10), 5);
+    const order = new Order(validCPF, new Coupon("Black Friday", CouponType.Percentage, 50, expiredDate));
+    order.addItem(PRODUCT_1_COSTING_15, 2);
+    order.addItem(PRODUCT_2_COSTING_8);
+    order.addItem(PRODUCT_3_COSTING_10, 5);
     expect(order.getTotal()).toBe(88);
 });
 
 test("Should give discount for an expired fixed discount coupon", () => {
-    const cpf_input = "558.155.780-29";
-    expirationDate.setFullYear(expirationDate.getFullYear() - 1);
-    const order = new Order(new CPF(cpf_input), new Coupon("Black Friday", CouponType.Fixed, 50, expirationDate));
-    order.addItem(new Item("gasolina", 666, 15), 2);
-    order.addItem(new Item("arroz", 193, 8));
-    order.addItem(new Item("feijao", 200, 10), 5);
+    const order = new Order(validCPF, new Coupon("Black Friday", CouponType.Fixed, 50, expiredDate));
+    order.addItem(PRODUCT_1_COSTING_15, 2);
+    order.addItem(PRODUCT_2_COSTING_8);
+    order.addItem(PRODUCT_3_COSTING_10, 5);
     expect(order.getTotal()).toBe(88);
 });
